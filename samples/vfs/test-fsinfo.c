fae92986eebe2d36a46d5d5c5d4d01ec8322922e
David Howells <dhowells@redhat.com>
2019-07-05 11:10:10 +0100

    fsinfo: Allow fsinfo() to look up a mount object by ID
    Allow the fsinfo() syscall to look up a mount object by ID rather than by
    pathname.  This is necessary as there can be multiple mounts stacked up at
    the same pathname and there's no way to look through them otherwise.
    
    This is done by passing FSINFO_FLAGS_QUERY_MOUNT to fsinfo() in the
    parameters and then passing the mount ID as a string to fsinfo() in place
    of the filename:
    
    	struct fsinfo_params params = {
    		.flags	 = FSINFO_FLAGS_QUERY_MOUNT,
    		.request = FSINFO_ATTR_IDS,
    	};
    
    	ret = fsinfo(AT_FDCWD, "21", &params, buffer, sizeof(buffer));
    
    The caller is only permitted to query a mount object if the root directory
    of that mount connects directly to the current chroot if dfd == AT_FDCWD[*]
    or the directory specified by dfd otherwise.  Note that this is not
    available to the pathwalk of any other syscall.
    
    [*] This needs to be something other than AT_FDCWD, perhaps AT_FDROOT.
    
    [!] This probably needs an LSM hook.
    
    [!] This might want to check the permissions on all the intervening dirs -
        but it would have to do that under RCU conditions.
    
    [!] This might want to check a CAP_* flag.
    
    Signed-off-by: David Howells <dhowells@redhat.com>

--- samples/vfs/test-fsinfo.c
+++ samples/vfs/test-fsinfo.c
@@ -592,7 +592,7 @@ int main(int argc, char **argv)
 	bool meta = false;
 	int raw = 0, opt, Nth, Mth;
 
-	while ((opt = getopt(argc, argv, "Madlr"))) {
+	while ((opt = getopt(argc, argv, "Madmlr"))) {
 		switch (opt) {
 		case 'M':
 			meta = true;
@@ -608,6 +608,10 @@ int main(int argc, char **argv)
 			params.at_flags &= ~AT_SYMLINK_NOFOLLOW;
 			params.flags = FSINFO_FLAGS_QUERY_PATH;
 			continue;
+		case 'm':
+			params.resolve_flags = 0;
+			params.flags = FSINFO_FLAGS_QUERY_MOUNT;
+			continue;
 		case 'r':
 			raw = 1;
 			continue;
@@ -620,6 +624,7 @@ int main(int argc, char **argv)
 
 	if (argc != 1) {
 		printf("Format: test-fsinfo [-Madlr] <path>\n");
+		printf("Format: test-fsinfo [-Mdr] -m <mnt_id>\n");
 		exit(2);
 	}
 
