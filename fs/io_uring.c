93fa762436bd4b9b1ed94e38d0c9ec45eed35a85
Jens Axboe <axboe@kernel.dk>
2020-04-28 13:16:53 -0600

    io_uring: only force async punt if poll based retry can't handle it
    We do blocking retry from our poll handler, if the file supports polled
    notifications. Only mark the request as needing an async worker if we
    can't poll for it.
    
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -2601,7 +2601,8 @@ copy_iov:
 			if (ret)
 				goto out_free;
 			/* any defer here is final, must blocking retry */
-			if (!(req->flags & REQ_F_NOWAIT))
+			if (!(req->flags & REQ_F_NOWAIT) &&
+			    !file_can_poll(req->file))
 				req->flags |= REQ_F_MUST_PUNT;
 			return -EAGAIN;
 		}
@@ -2723,7 +2724,8 @@ copy_iov:
 			if (ret)
 				goto out_free;
 			/* any defer here is final, must blocking retry */
-			req->flags |= REQ_F_MUST_PUNT;
+			if (!file_can_poll(req->file))
+				req->flags |= REQ_F_MUST_PUNT;
 			return -EAGAIN;
 		}
 	}
